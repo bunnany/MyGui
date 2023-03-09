a2fe684a2387dcaa9327aad7871b87a61a266efc
bunnany <45374304+bunnany@users.noreply.github.com>
2023-03-09 15:11:42 +1300

    implement clicks and colours
    responds to mouse clicks to draw a shape, a button to choose a random colour and choose a colour through the swing library colour swatch

--- MyGui.java
+++ MyGui.java
@@ -1,5 +1,6 @@
 import ecs100.*;
 import java.awt.Color;
+import javax.swing.JColorChooser;
 /**
  * Making some sliders and responding to mouse events
  *
@@ -14,6 +15,9 @@ public class MyGui
     // fields to remember "pressed" position
     private double startX, startY;
 
+    // Remember the colour
+    private Color currentColor = Color.black;
+    
     /**
      * Constructor for objects of class MyGui
      */
@@ -24,6 +28,10 @@ public class MyGui
         
         // setup buttons
         UI.addButton("Quit", UI::quit);
+        
+        // Colour Buttons
+        UI.addButton("Colour", this::chooseColour);
+        UI.addButton("Random Colour", this::changeColour);
         
         // setup slider
         UI.addSlider("Speed", 0, 100, 20, this::setSpeed);
@@ -55,11 +63,32 @@ public class MyGui
      * Only make one callback method to the mouse listener!
      */
     public void doMouse(String action, double x, double y) {
-        if (action.equals("pressed")) {
+        double width = 50;
+        double height = 50;
+        if (action.equals("clicked")) {
+            UI.fillOval(x-width/2, y-height/2, width, height);
+        } else if (action.equals("pressed")) {
             this.startX = x;
             this.startY = y;
         } else if (action.equals("released")) {
             UI.drawLine(this.startX, this.startY, x, y);
         }
+    }
+    
+    /**
+     * Change to a random Colour
+     */
+    public void changeColour() {
+        //RGB values
+        Color col = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+        UI.setColor(col);
+    }
+    
+    /**
+     * Allows the user to choose a colour using the swing library
+     */
+    public void chooseColour() {
+        this.currentColor = JColorChooser.showDialog(null, "Choose Color", this.currentColor);
+        UI.setColor(this.currentColor);
     }
 }
